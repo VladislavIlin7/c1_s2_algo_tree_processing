8944ef1c302e8351e48f84498e10274c755eb81a
VladislavIlin7 <svladilin@gmail.com>
2026-02-26 00:17:03 +0300

    add all func

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 typedef struct node {
     int data;
     struct node *left;
     struct node *right;
-}node;
+} node;
 
-node* create_tree(int data) {
+node* createTree(int data) {
     node *newNode = (node*)malloc(sizeof(node));
     if (newNode == NULL) {
         printf("Memory allocation error");
@@ -22,7 +21,7 @@ node* create_tree(int data) {
 
 node* insert(node *root, int data) {
     if (root == NULL) {
-        return create_tree(data);
+        return createTree(data);
     }
     if (data < root->data) {
         root->left = insert(root->left, data);
@@ -31,29 +30,28 @@ node* insert(node *root, int data) {
         root->right = insert(root->right, data);
     }
     return root;
-
 }
 
-node* findMin(node *val) {
-    if (val == NULL) {
+node* findMin(node *root) {
+    if (root == NULL) {
         return NULL;
     }
-    node *current = val;
+    node *current = root;
     while (current->left != NULL) {
         current = current->left;
     }
     return current;
 }
 
-node* delete(node *root, int data) {
+node* deleteNode(node *root, int data) {
     if (root == NULL) {
         return root;
     }
     if (data < root->data) {
-        root->left = delete(root->left, data);
+        root->left = deleteNode(root->left, data);
     }
     else if (data > root->data) {
-        root->right = delete(root->right, data);
+        root->right = deleteNode(root->right, data);
     }
     else {
         if (root->left == NULL) {
@@ -68,12 +66,74 @@ node* delete(node *root, int data) {
         }
         node *temp = findMin(root->right);
         root->data = temp->data;
-        root->right = delete(root->right, temp->data);
+        root->right = deleteNode(root->right, temp->data);
     }
     return root;
 }
 
+void printTree(node *root, int level) {
+    if (root == NULL) {
+        return;
+    }
+    for (int i = 0; i < level; i++) {
+        printf("    ");
+    }
+    printf("|-- %d\n", root->data);
+
+    printTree(root->left, level + 1);
+    printTree(root->right, level + 1);
+}
+
+int getDegree(node *node) {
+    if (node == NULL) {
+        return 0;
+    }
+    int degree = 0;
+    if (node->left != NULL) {
+        degree++;
+    }
+    if (node->right != NULL) {
+        degree++;
+    }
+    return degree;
+}
+
+int countVerticesWithSameDegree(node *root) {
+    if (root == NULL) {
+        return 0;
+    }
+    int count = 0;
+    if (root->data == getDegree(root)) {
+        count++;
+    }
+    return count + countVerticesWithSameDegree(root->left) +
+           countVerticesWithSameDegree(root->right);
+}
+
+void freeTree(node *root) {
+    if (root == NULL) {
+        return;
+    }
+    freeTree(root->left);
+    freeTree(root->right);
+    free(root);
+}
+
 int main(void) {
-    printf("Hello, World!\n");
+    node *root = NULL;
+    root = insert(root, 850);
+    root = insert(root, 900);
+    root = insert(root, 8000);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 60);
+    root = insert(root, 300);
+    root = insert(root, 35);
+    root = insert(root, 22);
+
+    printTree(root, 0);
+
+    freeTree(root);
+
     return 0;
 }
