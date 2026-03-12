d544edad7d3225270c885eaf5b8a1f070140617e
VladislavIlin7 <svladilin@gmail.com>
2026-03-12 23:26:02 +0300

    some changes

--- lab2/table.h
+++ lab2/table.h
@@ -10,11 +10,11 @@ typedef struct Table{
 
 void initTable(Table *table);
 int key_cmp(int a_int, char a_char, int b_int, char b_char);
-void addElement(Table *table,int key_int, char key_char, char *value);
-void readTableFromFile(Table* table, const char* filename);
+void addElement(Table *table, int key_int, char key_char, const char *value);
+void readTableFromFile(Table *table, const char *filename);
 void selectionSort(Table *t);
-int binarySearch(Table *table, int key_int, char key_char);
-void printFindElement(Table *table, int index);
+int binarySearch(const Table *table, int key_int, char key_char);
+void printFindElement(const Table *table, int index);
 void printTable(const Table *table);
 void freeTable(Table *table);
 #endif
